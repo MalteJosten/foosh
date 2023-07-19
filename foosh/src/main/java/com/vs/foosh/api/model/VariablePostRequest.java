4dffcd4454e97d28f5e22ba353581ab12e04f141
Malte Josten <malte.josten@gmail.com>
2023-07-19 12:00:55 +0200

    Created VariablePostRequest

--- foosh/src/main/java/com/vs/foosh/api/model/VariablePostRequest.java
+++ foosh/src/main/java/com/vs/foosh/api/model/VariablePostRequest.java
@@ -1,5 +1,28 @@
 package com.vs.foosh.api.model;
 
+import java.util.List;
+import java.util.UUID;
+
 public class VariablePostRequest {
+    private String name;
+    private List<UUID> devices;
+    private List<UUID> models;
     
+    public VariablePostRequest(String name, List<UUID> devices, List<UUID> models) {
+        this.name    = name;
+        this.devices = devices;
+        this.models  = models;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public List<UUID> getDevices() {
+        return this.devices;
+    }
+
+    public List<UUID> getModels() {
+        return this.models;
+    }
 }
