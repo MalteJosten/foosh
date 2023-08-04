b615abb2970e3d041cd61c509b76f0d2bbf2d595
Malte Josten <malte.josten@gmail.com>
2023-08-04 14:20:01 +0200

    Added dummy code to test prediction. It seems to work :)

--- foosh/src/main/java/com/vs/foosh/custom/PredictionModelSR.java
+++ foosh/src/main/java/com/vs/foosh/custom/PredictionModelSR.java
@@ -1,11 +1,17 @@
 package com.vs.foosh.custom;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import com.vs.foosh.api.exceptions.variable.VariablePredictionException;
+import com.vs.foosh.api.model.device.AbstractDevice;
 import com.vs.foosh.api.model.predictionModel.AbstractPredictionModel;
+import com.vs.foosh.api.model.predictionModel.ParameterMapping;
 import com.vs.foosh.api.model.web.SmartHomeInstruction;
+import com.vs.foosh.api.services.ListService;
 
 public class PredictionModelSR extends AbstractPredictionModel {
 
@@ -17,8 +23,31 @@ public class PredictionModelSR extends AbstractPredictionModel {
     }
 
     @Override
-    public List<SmartHomeInstruction> makePrediction(String value) {
-        return new ArrayList<>();
+    public List<SmartHomeInstruction> makePrediction(UUID variableId, String value) {
+        List<ParameterMapping> mappings = getMappings(variableId);
+
+        List<SmartHomeInstruction> instructions = new ArrayList<>();
+        AbstractDevice device = ListService.getDeviceList().getThing(mappings.get(0).getDeviceId());
+        URI deviceSmartHomeURI;
+
+        try {
+            deviceSmartHomeURI = new URI(device.getDescription().getProperties().get("link").toString());
+
+            SmartHomeInstruction instruction = new SmartHomeInstruction(
+                1,
+                UUID.fromString(mappings.get(0).getDeviceId()),
+                "OFF",
+                deviceSmartHomeURI
+            );
+
+            instructions.add(instruction);
+
+        } catch (URISyntaxException e) {
+            throw new VariablePredictionException(variableId, "An error occurred while creating smart home instructions.");
+        }
+
+        return instructions;
+
     }
     
 }
