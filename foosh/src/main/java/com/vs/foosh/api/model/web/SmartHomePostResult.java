5db1d8d30c9367c3cf48d58fb924b60b453b7157
Malte Josten <malte.josten@gmail.com>
2023-08-07 13:49:13 +0200

    Made SmartHomePostResult a Record

--- foosh/src/main/java/com/vs/foosh/api/model/web/SmartHomePostResult.java
+++ foosh/src/main/java/com/vs/foosh/api/model/web/SmartHomePostResult.java
@@ -2,23 +2,7 @@ package com.vs.foosh.api.model.web;
 
 import org.springframework.http.HttpStatusCode;
 
-public class SmartHomePostResult {
-    private int index;
-    private HttpStatusCode statusCode;
-
-    public SmartHomePostResult(int index, HttpStatusCode statusCode) {
-        this.index      = index;
-        this.statusCode = statusCode;
-    }
-
-    public int getIndex() {
-        return this.index;
-    }
-
-    public HttpStatusCode getStatusCode() {
-        return this.statusCode;
-    }
-
+public record SmartHomePostResult(int index, HttpStatusCode statusCode) {
     @Override
     public String toString() {
         StringBuilder builder = new StringBuilder("<< SmartHomePostResult >>\n");
