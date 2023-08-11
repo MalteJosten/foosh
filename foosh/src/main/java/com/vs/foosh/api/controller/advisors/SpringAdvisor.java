8bac8e84508366af24b0bda50c439f6e32d64799
Malte Josten <malte.josten@gmail.com>
2023-08-11 12:03:25 +0200

    Added ProblemDetail á la RFC 7807 to SpringAdvisor

--- foosh/src/main/java/com/vs/foosh/api/controller/advisors/SpringAdvisor.java
+++ foosh/src/main/java/com/vs/foosh/api/controller/advisors/SpringAdvisor.java
@@ -3,8 +3,8 @@ package com.vs.foosh.api.controller.advisors;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.HttpMediaTypeNotAcceptableException;
@@ -15,8 +15,6 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import com.vs.foosh.api.services.HttpResponseBuilder;
-
 @ControllerAdvice
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class SpringAdvisor extends ResponseEntityExceptionHandler {
@@ -25,35 +23,32 @@ public class SpringAdvisor extends ResponseEntityExceptionHandler {
     /// Spring Boot Overrides
     ///
 
-    // TODO: Test each API Endpoint and make sure to cover all @RequestBody exceptions (implement custom handler methods below)
-    // TODO: Add links
-
     @Override
     protected ResponseEntity<Object> handleHttpMediaTypeNotSupported(HttpMediaTypeNotSupportedException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
-        return HttpResponseBuilder.buildException(
-                "This method only accepts requests with Content-Type: 'application/json'",
-                HttpStatus.BAD_REQUEST);
+        return ResponseEntity.status(status).body(ProblemDetail.forStatusAndDetail(
+            status,
+            "This method does not allow the Content-Type: '" + ex.getContentType() + "'. Please refer to the documentation for the allowed Content-Type(s)."));
     }
 
     @Override
 	protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
-        return HttpResponseBuilder.buildException(
-                "Failed to read request. Please refer to the documentation for the valid type of request(s).",
-                HttpStatus.BAD_REQUEST);
+        return ResponseEntity.status(status).body(ProblemDetail.forStatusAndDetail(
+            status,
+            "Failed to read request. Please refer to the documentation for the valid type of request(s)."));
     }
 
     @Override
     protected ResponseEntity<Object> handleHttpMediaTypeNotAcceptable(HttpMediaTypeNotAcceptableException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
-        return HttpResponseBuilder.buildException(
-                "This method only accepts requests with Content-Type: 'application/json'",
-                HttpStatus.BAD_REQUEST);
+        return ResponseEntity.status(status).body(ProblemDetail.forStatusAndDetail(
+            status,
+            "This method only accepts requests with Content-Type: 'application/json'"));
     }
 
-    @ExceptionHandler({ MethodArgumentTypeMismatchException.class })
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     public ResponseEntity<Object> handleMethodArgumentTypeMismatch(MethodArgumentTypeMismatchException ex, WebRequest request) {
-        return HttpResponseBuilder.buildException(
-                "The path argument '" + ex.getName() + "' should be of type " + ex.getRequiredType().getName(),
-                HttpStatus.BAD_REQUEST);
+        return ResponseEntity.status(400).body(ProblemDetail.forStatusAndDetail(
+            HttpStatusCode.valueOf(400),
+            "The path argument '" + ex.getName() + "' should be of type " + ex.getRequiredType().getName()));
     }
 
 }
