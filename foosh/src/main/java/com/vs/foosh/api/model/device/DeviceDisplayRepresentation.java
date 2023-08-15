fded5f820d3bd6f8005d3aae6f42c37e9ed5e739
Malte Josten <malte.josten@uni-due.de>
2023-08-15 19:40:56 +0200

    [DOC] DeviceDisplayRepresentation

--- foosh/src/main/java/com/vs/foosh/api/model/device/DeviceDisplayRepresentation.java
+++ foosh/src/main/java/com/vs/foosh/api/model/device/DeviceDisplayRepresentation.java
@@ -5,19 +5,45 @@ import java.util.List;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.vs.foosh.api.model.web.LinkEntry;
 
+/**
+ * A data type containing a {@link DeviceResponseObject} and a {@link List} of {@link LinkEntry}
+ * to be used in HTTP responses.
+ */
 public class DeviceDisplayRepresentation {
+    /**
+     * The {@link DeviceResponseObject} holding a "reduced" version of a device
+     */
     private DeviceResponseObject device;
+
+    /**
+     * The {@link List} of {@link LinkEntry} that holds the relevant link information for the current respond
+     */
     private List<LinkEntry> links;
 
+    /**
+     * Creates a {@code DeviceDisplayRepresentation} given an {@link AbstractDevice}.
+     * 
+     * @param device the {@link AbstractDevice} to construct the display representation from
+     */
     public DeviceDisplayRepresentation(AbstractDevice device) {
         this.device = new DeviceResponseObject(device);
         this.links  = device.getSelfLinks();
     }
 
+    /**
+     * Returns the representation's response object.
+     * 
+     * @return the representation's {@link DeviceResponseObject}
+     */
     public DeviceResponseObject getDevice() {
         return this.device;
     }
 
+    /**
+     * Return the list containigall relevant {@link LinkEntry} elements.
+     * 
+     * @apiNote The {@code @JsonProperty} annotation is used to rename the field to "_links" when deserializing the object into JSON.
+     */
     @JsonProperty("_links")
     public List<LinkEntry> getLinks() {
         return this.links;
