2a0129637016d6642cb752c41b7c500c6f4ef342
Malte Josten <malte.josten@gmail.com>
2023-08-17 11:31:48 +0200

    [DOC] DeviceResponseObject

--- foosh/src/main/java/com/vs/foosh/api/model/device/DeviceResponseObject.java
+++ foosh/src/main/java/com/vs/foosh/api/model/device/DeviceResponseObject.java
@@ -2,11 +2,21 @@ package com.vs.foosh.api.model.device;
 
 import com.vs.foosh.api.model.misc.Thing;
 
+/**
+ * A data component that is a subclass of {@link Thing} and only includes fields of {@link AbstractDevice} which are relevant when using a {@link AbstractDevice} in a HTTP response.
+ * 
+ * The relevant fields include the device's {@code id}, {@code name}, {@code deviceName}, {@code type}, and {@code description}.
+ */
 public class DeviceResponseObject extends Thing {
     private String deviceName;
     private String type;
     private AbstractDeviceDescription description;
 
+    /**
+     * Given a device, create the corresponding {@code DeviceResponseObject}.
+     *
+     * @param device the {@link AbstractDevice} to create the {@code DeviceResponseObject} from 
+     */
     public DeviceResponseObject(AbstractDevice device) {
         this.id          = device.getId();
         this.name        = device.getName();
@@ -15,14 +25,29 @@ public class DeviceResponseObject extends Thing {
         this.description = device.getDescription();
     }
 
+    /**
+     * Return the {@code deviceName}.
+     * 
+     * @return the {@code deviceName}
+     */
     public String getDeviceName() {
         return this.deviceName;
     }
 
+    /**
+     * Return the {@code type}.
+     * 
+     * @return the {@code type}
+     */
     public String getType() {
         return this.type;
     }
 
+    /**
+     * Return the {@code description}.
+     * 
+     * @return the {@code description}
+     */
     public AbstractDeviceDescription getDescription() {
         return this.description;
     }
