eb3025b116fe7794e0769e37ac2a9112652bc9f4
Malte Josten <malte.josten@gmail.com>
2023-08-17 11:44:12 +0200

    [DOC] FetchDeviceResponse

--- foosh/src/main/java/com/vs/foosh/api/model/device/FetchDeviceResponse.java
+++ foosh/src/main/java/com/vs/foosh/api/model/device/FetchDeviceResponse.java
@@ -2,30 +2,46 @@ package com.vs.foosh.api.model.device;
 
 import java.util.List;
 
+/**
+ * A data component containing a list with elements of type {@link AbstractDevice} and a boolean indicator {@code success} if fetching was successful.
+ */
 public class FetchDeviceResponse {
-    private String message;
+    /**
+     * A {@link List} with elements of type {@link AbstractDevice} containing the list of fetched devices.
+     */
     private List<AbstractDevice> devices;
+
+    /**
+     * A boolean to indicate, if the fetching process was successful.
+     */
     private boolean success;
 
-    public FetchDeviceResponse( List<AbstractDevice> devices, boolean success) {
+    /**
+     * Create a {@code FetchDeviceResponse} given a a list of devices and a success indicator.
+     * 
+     * @param devices the {@link List} with elements of type {@link AbstractDevices}
+     * @param success a boolean value indicating whether the fetching process was successful or not
+     */
+    public FetchDeviceResponse(List<AbstractDevice> devices, boolean success) {
         this.devices = devices;
         this.success = success;
     }
 
-    public FetchDeviceResponse(String message, List<AbstractDevice> devices, boolean success) {
-        this.message = message;
-        this.devices = devices;
-        this.success = success;
-    }
-
-    public String getMessage() {
-        return this.message;
-    }
-
+    /**
+     * Return the list of fetched devices.
+     * 
+     * @return the {@link List} with elements of type {@link AbstractDevice}
+     */
     public List<AbstractDevice> getDevices() {
         return this.devices;
     }
 
+    /**
+     * Return {@code true} if the fetching process was successful.
+     * Return {@code false} if the fetching process was unsuccessful.
+     * 
+     * @return the value of {@code success}
+     */
     public boolean getSucess() {
         return this.success;
     }
@@ -33,7 +49,6 @@ public class FetchDeviceResponse {
     @Override
     public String toString() {
         StringBuilder builder = new StringBuilder("<< FetchDeviceResponse >>\n");
-        builder.append("Message: " + message + "\n");
         builder.append("Devices: " + devices + "\n");
         builder.append("Success: " + success);
 
