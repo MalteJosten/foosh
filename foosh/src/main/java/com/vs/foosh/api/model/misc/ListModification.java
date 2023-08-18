935574a4be1589faef8d997969a9027e54fe89b6
Malte Josten <malte.josten@gmail.com>
2023-08-18 08:14:08 +0200

    [DOC] ListModification

--- foosh/src/main/java/com/vs/foosh/api/model/misc/ListModification.java
+++ foosh/src/main/java/com/vs/foosh/api/model/misc/ListModification.java
@@ -1,7 +1,15 @@
 package com.vs.foosh.api.model.misc;
 
+/**
+ * A subclass of {@link AbstractModification} to be used by {@link IThingList}s.
+ */
 public class ListModification extends AbstractModification {
 
+    /**
+     * Create a {@code ListModification}, given a {@link ModificationType}.
+     * 
+     * @param modificationType the {@link ModificationType} that describes the list modification
+     */
     public ListModification(ModificationType modificationType) {
         super(modificationType);
     }
