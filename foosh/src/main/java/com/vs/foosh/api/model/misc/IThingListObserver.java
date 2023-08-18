e0e3f22d7d7e5bf73fe6acd822ad3b07d5dc40fd
Malte Josten <malte.josten@gmail.com>
2023-08-18 08:04:49 +0200

    [DOC] IThingObserver

--- foosh/src/main/java/com/vs/foosh/api/model/misc/IThingListObserver.java
+++ foosh/src/main/java/com/vs/foosh/api/model/misc/IThingListObserver.java
@@ -1,5 +1,15 @@
 package com.vs.foosh.api.model.misc;
 
+/**
+ * An interface providing needed capabilities for an observer.
+ * 
+ * @see <a href="https://refactoring.guru/design-patterns/observer">Observer Pattern</a>
+ */
 public interface IThingListObserver {
+    /**
+     * This method should be called by the subject to inform the observer about changes.
+     * 
+     * @param modification the {@link AbstractModification} describing the reason for calling the method
+     */
     public void update(AbstractModification modification); 
 }
