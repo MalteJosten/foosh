3434d4a9c837e5344fef75e57d9ba2321a38008d
Malte Josten <malte.josten@gmail.com>
2023-08-18 07:33:36 +0200

    [DOC] AbstractModification

--- foosh/src/main/java/com/vs/foosh/api/model/misc/AbstractModification.java
+++ foosh/src/main/java/com/vs/foosh/api/model/misc/AbstractModification.java
@@ -1,12 +1,32 @@
 package com.vs.foosh.api.model.misc;
 
+/**
+ * A container for a {@link ModificationType}.
+ * It is used for indicating the type of change/modification when communicating with an observer.
+ * 
+ * @see com.vs.foosh.api.model.misc.IThingListObserver
+ * @see com.vs.foosh.api.model.misc.IThingListSubject
+ */
 public abstract class AbstractModification {
+    /**
+     * The {@link ModificationType} of this modification.
+     */
     protected ModificationType modificationType;
 
+    /**
+     * Create a new {@code AbstractModification} with the given {@code modificationType}.
+     * 
+     * @param modificationType the {@link ModificationType}
+     */
     public AbstractModification(ModificationType modificationType) {
         this.modificationType = modificationType;
     }
     
+    /**
+     * Return the modification type.
+     * 
+     * @return the {@code modificationType} as {@link ModificationType}
+     */
     public ModificationType getModificationType() {
         return this.modificationType;
     }
