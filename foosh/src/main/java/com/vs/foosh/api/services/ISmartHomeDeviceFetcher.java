536c439b1779e21fbf9e84cad7a329e77fec6a96
Malte Josten <malte.josten@gmail.com>
2023-08-30 10:43:46 +0200

    Added @Service to SmartHome Interfaces

--- foosh/src/main/java/com/vs/foosh/api/services/ISmartHomeDeviceFetcher.java
+++ foosh/src/main/java/com/vs/foosh/api/services/ISmartHomeDeviceFetcher.java
@@ -2,11 +2,13 @@ package com.vs.foosh.api.services;
 
 import java.io.IOException;
 
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.ResourceAccessException;
 
 import com.vs.foosh.api.model.device.FetchDeviceResponse;
 import com.vs.foosh.api.model.web.SmartHomeDetails;
 
+@Service
 public interface ISmartHomeDeviceFetcher {
     public FetchDeviceResponse fetchDevicesFromSmartHomeAPI(SmartHomeDetails details) throws ResourceAccessException, IOException;
 }
--- foosh/src/main/java/com/vs/foosh/api/services/ISmartHomeInstructionExecutor.java
+++ foosh/src/main/java/com/vs/foosh/api/services/ISmartHomeInstructionExecutor.java
@@ -2,9 +2,12 @@ package com.vs.foosh.api.services;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import com.vs.foosh.api.model.web.SmartHomeInstruction;
 import com.vs.foosh.api.model.web.SmartHomePostResult;
 
+@Service
 public interface ISmartHomeInstructionExecutor {
     public  List<SmartHomePostResult> sendAndExecuteSmartHomeInstructions(List<SmartHomeInstruction> instructions);
 }
