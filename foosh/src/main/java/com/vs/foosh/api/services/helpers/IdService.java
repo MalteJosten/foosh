0a7ec079c15cbe2fdf5d72257a3893f5e75263bd
Malte Josten <malte.josten@gmail.com>
2023-08-31 15:08:01 +0200

    [DOC] IdService

--- foosh/src/main/java/com/vs/foosh/api/services/helpers/IdService.java
+++ foosh/src/main/java/com/vs/foosh/api/services/helpers/IdService.java
@@ -8,8 +8,18 @@ import org.springframework.stereotype.Service;
 
 import com.vs.foosh.api.model.misc.Thing;
 
+/**
+ * A {@link Service} that provides some simple functions to work with IDs and lists.
+ */
 @Service
 public class IdService {
+    /**
+     * Checks whether a provided {@link String} could be a valid {@link UUID}.
+     * 
+     * @param id the {@link String} under test
+     * @return Optional.empyt(), if {@code id} could not be an {@link UUID}
+     * @return Optional.of(UUID), if {@code id} could be an {@link UUID}
+     */
     public static Optional<UUID> isUuid(String id) {
         UUID uuid;
         try {
@@ -21,6 +31,14 @@ public class IdService {
         return Optional.of(uuid);
     }
 
+    /**
+     * Check whether a given identifier is an identifier of a {@link Thing} (either a its {@code id} or {@code name}) in a {@link List} of {@link Thing}s.
+     * 
+     * @param id the {@link String} identifier to check the existence of
+     * @param collection the {@link List} of {@link Thing}s to search for the {@code id}
+     * 
+     * @return {@code true} if there is a {@link Thing} in {@code collection} with the identifier {@code id}
+     */
     public static <T extends Thing> boolean isIdentifierInList(String id, List<T> collection) {
         if (collection == null) {
             return false;
@@ -35,6 +53,14 @@ public class IdService {
         return false;
     }
 
+    /**
+     * Check whether a given {@link UUID} is an identifier of a {@link Thing} (its {@code id}) in a {@link List} of {@link Thing}s.
+     * 
+     * @param id the {@link UUID} identifier to check the existence of
+     * @param collection the {@link List} of {@link Thing}s to search for the {@code id}
+     * 
+     * @return {@code true} if there is a {@link Thing} in {@code collection} with the identifier {@code id}
+     */
     public static <T extends Thing> boolean isUuidInList(UUID id, List<T> collection) {
         for(T element: collection) {
             if (element.getId().equals(id)) {
