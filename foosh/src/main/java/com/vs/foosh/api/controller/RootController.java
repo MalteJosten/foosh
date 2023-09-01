0446bcd10b6be1f244a8aa75d06ba700d252a24f
Malte Josten <malte.josten@uni-due.de>
2023-09-01 17:24:08 +0200

    [DOC] RootController

--- foosh/src/main/java/com/vs/foosh/api/controller/RootController.java
+++ foosh/src/main/java/com/vs/foosh/api/controller/RootController.java
@@ -11,14 +11,32 @@ import org.springframework.web.servlet.view.RedirectView;
 
 import com.vs.foosh.api.services.helpers.LinkBuilderService;
 
+/**
+ * A {@link RestController} that handles HTTP requests for the routes {@code /} and {@code /api}.
+ */
 @RestController
 public class RootController {
 
+    /**
+     * Handle incoming {@code GET} requests on route {@code /} using {@link GetMapping}.
+     * 
+     * Redirect to {@code /api}.
+     * 
+     * @return a {@link RedirectView} with the redirection URL "{@code /api}"
+     */
     @GetMapping("/")
     public RedirectView index() {
         return new RedirectView("/api");
     }
 
+    /**
+     * Handle incoming {@code GET} requests on route {@code /api} using {@link GetMapping}.
+     * 
+     * Response with the appliction's version and the list of available "root" routes.
+     * @see {@link com.vs.foosh.api.services.helpers.LinkBuilderService#getRootLinkEntries() getRootLinkEntries()}
+     * 
+     * @return a {@link ResponseEntity} containing the version and list available routes
+     */
     @GetMapping("/api")
     public ResponseEntity<Object> apiIndex() {
         Map<String, Object> responseBody = new HashMap<>();
