773918e1a0aa9647ac05f719fd9b000fe9dceb75
Malte Josten <malte.josten@uni-due.de>
2023-09-01 17:24:02 +0200

    [DOC] FooSHErrorController

--- foosh/src/main/java/com/vs/foosh/api/controller/FooshErrorController.java
+++ foosh/src/main/java/com/vs/foosh/api/controller/FooshErrorController.java
@@ -1,10 +1,8 @@
 package com.vs.foosh.api.controller;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,9 +12,20 @@ import com.vs.foosh.api.services.helpers.LinkBuilderService;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
 
+/**
+ * A {@link Controller} that extends Spring's {@link ErrorController} and replaces the standard Spring Whitelabel Error Page ({@code /error}).
+ * 
+ * For this to properly work, {@code server.error.whitelabel.enabled} should be set to {@code false} in {@code application.properties}.
+ */
 @Controller
 public class FooshErrorController implements ErrorController {
 
+    /**
+     * Handle all not handled exceptions/errors and display a fitting {@link ProblemDetail}.
+     * @see <a href="https://www.rfc-editor.org/rfc/rfc7807">RFC 7807</a>
+     * 
+     * @return a {@link ResponseEntity} with the constructed {@link ProblemDetail} as its body
+     */
     @RequestMapping("/error")
     public ResponseEntity<Object> handleError(HttpServletRequest request) {
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
@@ -32,11 +41,10 @@ public class FooshErrorController implements ErrorController {
             }
         }
 
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(httpStatus, message);
+        problemDetail.setDetail(message);
+        problemDetail.setProperty("_links", LinkBuilderService.getRootLinkEntries());
 
-        Map<String, Object> responseBody = new HashMap<>();
-        responseBody.put("message", message);
-        responseBody.put("_links", LinkBuilderService.getRootLinkEntries());
-
-        return new ResponseEntity<>(responseBody, httpStatus);
+        return ResponseEntity.status(httpStatus).body(problemDetail);
     }
 }
